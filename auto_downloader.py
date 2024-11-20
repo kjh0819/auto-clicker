0257b144ec70e3e164b1674165b627cd4c5fdd41
kjh0819 <107752598+kjh0819@users.noreply.github.com>
2024-11-20 17:05:36 +0900

    Add Extra Image

--- auto_downloader.py
+++ auto_downloader.py
@@ -1,14 +1,17 @@
 import os
+os.system("pip install pyautogui")
+os.system("pip install opencv-python")
+os.system("pip install pillow")
 import pyautogui
 import time
 
 print(os.getcwd())
 
-pyautogui.FAILSAFE = False
-
 # Image file paths
-launcher_download_path = "download_launcher.PNG"
-website_download_path = "download_website.PNG"
+launcher_download_path = "LauncherDownload1.png"
+LDP2= "LauncherDownload2.png"
+website_download_path = "download_website1.PNG"
+WDP2="download_website2.png"
 threshold = 0.9
 
 while True:
@@ -20,13 +23,22 @@ while True:
             # Click on the launcher download button if found
             pyautogui.click(launcher_location.left + 5, launcher_location.top + 5)
 
-        # Introduce a delay before taking the screenshot
-        time.sleep(3)
-    
+        time.sleep(3)  # Introduce a delay before taking the screenshot
+
     except pyautogui.ImageNotFoundException:
-        # Handle the exception (e.g., print a message)
-        print("Image not found. Waiting for the element to appear.")
-        time.sleep(3)  # Add a delay before retrying
+        try:
+            print("Checking for Secondary Launcher Download Button")
+            launcher_location = pyautogui.locateOnScreen(LDP2, confidence=threshold)
+
+            if launcher_location is not None:
+                # Click on the secondary launcher download button if found
+                pyautogui.click(launcher_location.left + 10, launcher_location.top + 10)
+
+            time.sleep(3)  # Introduce a delay before taking the screenshot
+
+        except pyautogui.ImageNotFoundException::
+            print("Secondary image not found. Waiting for the element to appear.")
+            
 
     try:
         print("Checking for Website Download Button")
@@ -38,7 +50,17 @@ while True:
 
         time.sleep(3)
         print("Waiting for 5 seconds...")
+
     except pyautogui.ImageNotFoundException:
-        # Handle the exception (e.g., print a message)
-        print("Image not found. Waiting for the element to appear.")
-        time.sleep(3)  # Add a delay before retrying
+        try:
+            print("Checking for Secondary Launcher Download Button")
+            launcher_location = pyautogui.locateOnScreen(WDP2, confidence=threshold)
+
+            if launcher_location is not None:
+                # Click on the secondary launcher download button if found
+                pyautogui.click(launcher_location.left + 5, launcher_location.top + 5)
+
+            time.sleep(3)  # Introduce a delay before taking the screenshot
+
+        except pyautogui.ImageNotFoundException:
+            print("Secondary image not found. Waiting for the element to appear.")
